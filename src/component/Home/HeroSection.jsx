a810456e8868bfc559762c0c0135d59cbf71892b
shoba rohra <rshoba822@gmail.com>
2024-12-17 22:37:44 +0500

    navbar

--- src/component/Home/HeroSection.jsx
+++ src/component/Home/HeroSection.jsx
@@ -10,9 +10,10 @@ function HeroSection() {
           Transforming Ideas into{' '}
           <span className='highlight'>Digital Reality</span>
         </h1>
-        <p>We are a passionate team of software developers specializing</p>
-        <p> in Web Development, Mobile App Development, and AI-</p>
-        <p> Powered Solutions.</p>
+        <p>
+          We are a passionate team of software developers specializing in Web
+          Development, Mobile App Development, and AI-Powered Solutions.
+        </p>
         <button className='hero-btn'>Let's get started!</button>
       </div>
       <img src={img1} alt='Hero' className='hero-image' />
